bf6e995d5c47b623b8bd5ec7c2d727d47d0637aa
Anita <09anita07choudhary@gmail.com>
2022-04-01 15:51:15 +0530

    NewsApp using React.js

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import './App.css';
+
+import React, { useState } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar'
+
+const App = ()=> {
+  const pageSize = 5;
+   const apiKey = process.env.REACT_APP_NEWS_API
+  const [progress, setProgress] = useState(0)
+ 
+    return (
+      <div>
+        <Router >
+        <Navbar/> 
+        <LoadingBar
+        height={3}
+        color='#f11946'
+        progress={progress} 
+      />
+        <Routes>
+          <Route exact path="/"><News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general"/></Route> 
+          <Route exact path="/business"><News setProgress={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country="in" category="business"/></Route> 
+          <Route exact path="/entertainment"><News setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country="in" category="entertainment"/></Route> 
+          <Route exact path="/general"><News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general"/></Route> 
+          <Route exact path="/health"><News setProgress={setProgress} apiKey={apiKey} key="health" pageSize={pageSize} country="in" category="health"/></Route> 
+          <Route exact path="/science"><News setProgress={setProgress} apiKey={apiKey} key="science" pageSize={pageSize} country="in" category="science"/></Route> 
+          <Route exact path="/sports"><News setProgress={setProgress} apiKey={apiKey} key="sports" pageSize={pageSize} country="in" category="sports"/></Route> 
+          <Route exact path="/technology"><News setProgress={setProgress} apiKey={apiKey} key="technology" pageSize={pageSize} country="in" category="technology"/></Route> 
+        </Routes>
+        </Router>
+      </div>
+    )
+ 
+}
+
+export default App;
+
+// import React, { Component } from 'react'
+// import Navbar from './components/Navbar';
+// import News from './components/News';
+
+//  export default class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <Navbar />
+//         <News pageSize = {5} />
+//       </div>
+//     )
+//   }
+// }
+
+// export default App
